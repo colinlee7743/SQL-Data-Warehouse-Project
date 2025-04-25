e92d57aaa5439896c009d96b402cd6c445ca1d16
Colin Lee <colinlee7743@gmail.com>
2025-04-25 15:50:11 +1000

    Update 05_data_analysis.sql

--- scripts/05_data_analysis.sql
+++ scripts/05_data_analysis.sql
@@ -23,7 +23,7 @@ SQL Functions Used:
 -- =============================================================================
 -- Analyse sales performance over year
 SELECT
-	YEAR(order_date) as order_year,
+    YEAR(order_date) as order_year,
     SUM(sales_amount) as total_sales,
     SUM(quantity) as quantity,
     COUNT(DISTINCT customer_id) as total_customers
@@ -34,7 +34,7 @@ ORDER BY YEAR(order_date);
 
 -- Analyse sales performance over month
 SELECT
-	DATE_FORMAT(order_date, '%Y%m') as order_date,
+    DATE_FORMAT(order_date, '%Y%m') as order_date,
     SUM(sales_amount) as total_sales,
     SUM(quantity) as quantity,
     COUNT(DISTINCT customer_id) as total_customers
@@ -48,15 +48,15 @@ ORDER BY DATE_FORMAT(order_date, '%Y%m');
 -- =============================================================================
 -- Calculate the total sales per month and the running total of sales over time 
 SELECT 
-	order_year,
+    order_year,
     order_month,
     total_sales,
     sum(total_sales) OVER (PARTITION BY order_year ORDER BY order_month) AS running_sales
 FROM (
     SELECT
-		YEAR(order_date) as order_year,
+	YEAR(order_date) as order_year,
         DATE_FORMAT(order_date, '%Y%m') as order_month,
-		SUM(sales_amount) as total_sales
+	SUM(sales_amount) as total_sales
 	FROM fact_sales
 	WHERE order_date IS NOT NULL
 	GROUP BY YEAR(order_date), DATE_FORMAT(order_date, '%Y%m')
@@ -70,7 +70,7 @@ FROM (
 -- to both the average sales performance of the product and the previous year's sales
 WITH yearly_product_sales AS
 (SELECT
-	YEAR(f.order_date) as order_year,
+    YEAR(f.order_date) as order_year,
     p.product_name,
     SUM(f.sales_amount) as current_sales
 FROM fact_sales f
@@ -79,13 +79,13 @@ WHERE order_date IS NOT NULL
 GROUP BY YEAR(f.order_date), p.product_name
 )
 SELECT 
-	order_year,
+    order_year,
     product_name,
     current_sales,
     ROUND(avg(current_sales) OVER (PARTITION BY product_name), 0) as avg_sales,
     ROUND(current_sales - avg(current_sales) OVER (PARTITION BY product_name), 0) as diff_avg,
     CASE 
-		WHEN current_sales - avg(current_sales) OVER (PARTITION BY product_name) > 0 THEN 'Above Avg'
+	WHEN current_sales - avg(current_sales) OVER (PARTITION BY product_name) > 0 THEN 'Above Avg'
         WHEN current_sales - avg(current_sales) OVER (PARTITION BY product_name) < 0 THEN 'Below Avg'
         ELSE 'Avg'
 	END AS avg_change,
@@ -94,7 +94,7 @@ SELECT
     LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) as py_sales,
     current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) as diff_py,
      CASE 
-		WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increase'
+	WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increase'
         WHEN current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decrease'
         ELSE 'No Change'
 	END AS py_change
@@ -107,14 +107,14 @@ ORDER BY product_name, order_year;
 -- Which categories contribute the most to overall sales?
 WITH category_sales AS (
 SELECT
-	p.category,
-	SUM(f.sales_amount) as total_sales
+    p.category,
+    SUM(f.sales_amount) as total_sales
 FROM fact_sales f
 LEFT JOIN dim_products p ON f.product_key = p.product_key
 GROUP BY p.category
 )
 SELECT
-	category,
+    category,
     total_sales,
     SUM(total_sales) OVER () as overall_sales,
     CONCAT(ROUND(total_sales / SUM(total_sales) OVER () * 100,2),'%') as sales_percentage
@@ -127,11 +127,11 @@ ORDER BY total_sales DESC;
 -- Segment products into cost ranges and count how many products fall into each segment
 WITH product_cost AS (
 SELECT
-	product_key,
-	product_name,
+    product_key,
+    product_name,
     cost,
     CASE
-		WHEN cost < 100 THEN 'Below 100'
+	WHEN cost < 100 THEN 'Below 100'
         WHEN cost BETWEEN 100 AND 500 THEN '100-500'
         WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
         ELSE 'Above 1000'
@@ -139,7 +139,7 @@ SELECT
 FROM dim_products
 )
 SELECT
-	cost_range,
+    cost_range,
     COUNT(*) AS total_products
 FROM product_cost
 GROUP BY cost_range
@@ -153,7 +153,7 @@ ORDER BY total_products DESC;
 
 WITH customer_record AS (
 SELECT 
-	c.customer_key,
+    c.customer_key,
     SUM(sales_amount) AS total_spending,
     MIN(order_date) AS first_order,
     MAX(order_date) AS last_order,
@@ -164,11 +164,11 @@ GROUP BY customer_key
 ORDER BY customer_key
 )
 SELECT
-	CASE 
-		WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
-		WHEN lifespan >= 12 AND total_spending <= 5000 THEN 'Regular'
-		ELSE 'New'
-	END AS cust_group,
+    CASE 
+	WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
+	WHEN lifespan >= 12 AND total_spending <= 5000 THEN 'Regular'
+	ELSE 'New'
+    END AS cust_group,
     COUNT(customer_key) as total_customers
 FROM customer_record
 GROUP BY cust_group
